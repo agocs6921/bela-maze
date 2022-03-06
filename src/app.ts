ebc0c9a2159a93691d2dfb594590f224ce3c69b9
human512 <agocs6921@gmail.com>
2022-03-06 08:30:24 +0100

    Added comments

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,45 @@ import { generate } from "./mazeGenerator"
 const deg2rad = (deg: number) => deg * (Math.PI/180)
 const rad2deg = (rad: number) => rad * (180/Math.PI)
 
+/**
+ * Camera to render the scene with with
+ */
 const CAMERA = new PerspectiveCamera(90, window.innerWidth / window.innerHeight)
-const SCENE = new Scene()
+/**
+ * The scene mentioned before
+ */
+const SCENE = new Scene() 
 
+/**
+ * The renderer used for rendering the scene
+ */
 const RENDERER = new WebGLRenderer({ antialias: true })
-RENDERER.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(RENDERER.domElement)
+RENDERER.setSize(window.innerWidth, window.innerHeight) // Setting the renderer size to the viewport to get full coverage
+document.body.appendChild(RENDERER.domElement) // Adding the renderer's canvas(the element that displays everything)
 
+// Needed for calculating delta because the builtin time for the animation loop is fucked, somehow
 let previousTime = new Date().getTime()
 RENDERER.setAnimationLoop(() => {
-    update(previousTime - new Date().getTime())
+    /**
+     * Calculating the difference, aka the delta(the time between the current frame and the previous frame)
+     * and then pass it in for the update function, just like in game engines
+     */
+    update(new Date().getTime() - previousTime)
     
-    RENDERER.render(SCENE, CAMERA)
+    RENDERER.render(SCENE, CAMERA) // Rendering the scene
 
+    // Setting it so we can calculate it again later
     previousTime = new Date().getTime()
 })
 
+// We need this because if we don't do this
+// the rendering will be fucked up
 window.addEventListener("resize", updateRenderer)
 
+/**
+ * Updates the renderer and camera aspects because
+ * it gets funky if we don't
+ */
 function updateRenderer() {
     CAMERA.aspect = window.innerWidth / window.innerHeight
     CAMERA.updateProjectionMatrix()
@@ -29,85 +50,126 @@ function updateRenderer() {
     RENDERER.setSize(window.innerWidth, window.innerHeight)
 }
 
+// Degrees so we can rotate it around the maze
 let degree = 0
+
+/**
+ * Updating things, duh
+ * @param delta time between the last frame and the current
+ */
 function update(delta: number) {
+    // We set the positions of the camera, so it goes in a circle around the maze
+    // You just need to calculate the points with sin and cos to get the position
+    // and then some offset so it actually rotates around the maze
     CAMERA.position.x = x / 1.5 * Math.sin(deg2rad(degree)) + x / 2
     CAMERA.position.z = y / 1.5 * Math.cos(deg2rad(degree)) + y / 2
 
-    //CAMERA.rotation.y = deg2rad(degree)
+    // The method name explains itself
     CAMERA.lookAt(new Vector3(x / 2, 0, y / 2))
 
+    // We set the skyboxes position to the camera's... why? idk
     SKYBOX.position.set(CAMERA.position.x, CAMERA.position.y, CAMERA.position.z)
 
+    // We add some to the degree, why? Because you can't just add to the position and expect the same result...
+    // you could but I have no patience for that
     degree += .5
 }
 
+// Dimensions for the maze
 const [x, y] = [8, 8]
 
+// Generating the maze, duh
 let maze = generate(x, y)
 
-CAMERA.position.x = x / 2
+// Setting the position of the camera so it is slightly above the maze
 CAMERA.position.y = ((x + y) / 2) * .5
-CAMERA.position.z = y
 
-// CAMERA.rotation.x = 0
-
+// We load bela
 let susmak = new TextureLoader().load("./img/susmak.png")
 
+// Make the material for the walls
 const WALLMATERIAL = new MeshBasicMaterial({
     color: 0xffffff,
     map: susmak,
     side: DoubleSide
 })
+// We get the wall geometry
 const WALLGEOMETRY = new PlaneGeometry(1, 1)
 
+/**
+ * The skybox, there's no need to explain this one
+ */
 const SKYBOX = new Mesh(new BoxGeometry(2000, 2000, 2000), new MeshBasicMaterial({
     color: 0xffffff,
     map: susmak,
     side: BackSide
 }))
-
+// We add the skybox to the scene
 SCENE.add(SKYBOX)
 
+// and we add the floor like this,
+// why? because we don't need its reference, manipulation not needed
 SCENE.add((() => {
+    // We create a plane with the same size as the maze and add bela
     let floor = new Mesh(new PlaneGeometry(x, y), WALLMATERIAL)
 
+    // Positioning the floor so it is below the maze
     floor.position.x = x / 2 - .5
     floor.position.y = -.5
     floor.position.z = y / 2 - .5
 
+    // By default, planes stand upward
+    // so we have to rotate it by 90 degrees
     floor.rotation.x = deg2rad(90)
 
     return floor
 })())
 
+// We generate the walls for each cell in the maze
 for (let i = 0; i < maze.length; i++) {
+    // The cell to generate
     let cell = maze[i]
     
+    // The walls so we can add them later when the loop is done
     let walls: Mesh[] = []
     
     if (cell.frontWall ) {
+        // The wall, with the plane geometry and bela
         let wall = new Mesh(WALLGEOMETRY, WALLMATERIAL)
+        // Setting the position so it generates like normally
         wall.position.set(cell.x, 0, cell.y - .5)
+        // Adding it to the walls
         walls.push(wall)
     }
     if ( cell.backWall ) {
+        // The wall, with the plane geometry and bela
         let wall = new Mesh(WALLGEOMETRY, WALLMATERIAL)
+        // Setting the position so it generates like normally
         wall.position.set(cell.x, 0, cell.y + .5)
+        // Adding it to the walls
         walls.push(wall)
     }
     if (cell.leftWall ) {
+        // The wall, with the plane geometry and bela
         let wall = new Mesh(WALLGEOMETRY, WALLMATERIAL)
+        // Setting the position so it generates like normally
         wall.position.set(cell.x - .5, 0, cell.y)
+        // Rotating it so it actually looks like a wall facing left
         wall.rotation.y = deg2rad(90)
+        // Adding it to the walls
         walls.push(wall)
     }
     if (cell.rightWall ) {
+        // The wall, with the plane geometry and bela
         let wall = new Mesh(WALLGEOMETRY, WALLMATERIAL)
+        // Setting the position so it generates like normally
         wall.position.set(cell.x + .5, 0, cell.y)
+        // Rotating it so it actually looks like a wall facing right
         wall.rotation.y = deg2rad(90)
+        // Adding it to the walls
         walls.push(wall)
     }
     
+    // Adding the walls in the scene
     SCENE.add(...walls)
 }
--- src/mazeGenerator.ts
+++ src/mazeGenerator.ts
@@ -1,10 +1,38 @@
+/**
+ * Generates a maze
+ * @param width Width of the maze
+ * @param height Height of the maze
+ * @param seed Seed used for the random number generator
+ * @returns Randomly generated maze
+ */
 export function generate(width: number, height: number, seed?: number) {
+    /**
+     * Cell class used for the algorithm
+     */
     class Cell {
+        /**
+         * Has it been visited?
+         * 
+         * **Used for the algorithm**
+         */
         public visited = false
+        /**
+         * Cell's neighboring cells
+         * [front, back, left, right]
+         * 
+         * **Used for the algorithm**
+         */
         public neighbors: [Cell, Cell, Cell, Cell]
 
+        /**
+         * Used for constructing a cell
+         * @param x x position
+         * @param y y position
+         * @param walls walls for the cell
+         */
         constructor(public x: number, public y: number, private walls: [boolean, boolean, boolean, boolean] = [true, true, true, true]) {}
 
+        // Bunch of these explain themselves
         get frontWall() {
             return this.walls[0]
         }
@@ -33,6 +61,11 @@ export function generate(width: number, height: number, seed?: number) {
             this.walls[3] = v
         }
 
+        /**
+         * Cell's unvisited neighbors
+         * 
+         * **Used for the algorithm**
+         */
         get unvisitedNeighbors() {
             return this.neighbors.filter((v) => {
                 if (v == null) return false
@@ -41,11 +74,21 @@ export function generate(width: number, height: number, seed?: number) {
             })
         }
 
+        /**
+         * Does the cell have unvisited neighbors?
+         * 
+         * **Used for the algorithm**
+         */
         get hasUnvisitedNeighbors() {
             return this.unvisitedNeighbors.length != 0
         }
     }
 
+    /**
+     * Random number generator I found online lmao
+     * @param seed a seed
+     * @returns a number generator for generating pseudo-random numbers
+     */
     function mulberry32(seed: number) {
         return function() {
             let cycle = seed += 0x6D2B79F5;
@@ -57,18 +100,26 @@ export function generate(width: number, height: number, seed?: number) {
         }
     }
 
-    const random = mulberry32(seed ?? Date.now())
+    // These explain themselves
+    const random = mulberry32(seed ?? Date.now()) // We used the seed or the current time if seed wasn't passed
     const randint = (min: number, max: number) => Math.floor(random() * (max - min) + min)
 
-    const getPosition = (x: number, y: number) => width * y + x
-    const getPositionFromIndex = (index: number): [number, number] => [index % width, Math.floor(index / width)]
+    // this is garbage
+    // const getPosition = (x: number, y: number) => width * y + x
+    // const getPositionFromIndex = (index: number): [number, number] => [index % width, Math.floor(index / width)]
 
-    let cells = Array(width)
+    /**
+     * Generating the cells at each position
+     */
+    let cells: Cell[][] = Array(width)
     .fill(undefined)
     .map((_, x) => Array(height)
         .fill(undefined)
         .map((_, y) => new Cell(x, y)))
 
+    /**
+     * Setting each cell's neighbors
+     */
     cells.forEach((line, x) => {
         line.forEach((v, y) => {
             v.neighbors = [
@@ -88,43 +139,68 @@ export function generate(width: number, height: number, seed?: number) {
         })
     })
 
+    /**
+     * Tracks the number of unvisited cells (subtracts one because at the beginning we already have a visited cell)
+     */
     let unvisited = width * height - 1
+    /**
+     * The current cell
+     */
     let currentCell = cells[0][0]
     currentCell.visited = true
 
+    /**
+     * The stack, used for backtracking
+     */
     let stack: Cell[] = []
 
+    // while there are unvisted cells
     while (unvisited != 0) {
+        // if the current cell has unvisited neighbors
         if (currentCell.hasUnvisitedNeighbors) {
+            /**
+             * current cell's unvisited neighbors
+             */
             let unvisitedNeighbors = currentCell.unvisitedNeighbors
 
+            /**
+             * chose one of its neighbors
+             */
             let chosenCell = unvisitedNeighbors[randint(0, unvisitedNeighbors.length)]
 
+            // Push it to the stack, so we can backtrack to it later
             stack.push(currentCell)
 
-            if (currentCell.y - chosenCell.y > 0) {
+            // Connecting the current and the chosen cell
+            // based on where the chosen cell is relative to the current cell
+            if (currentCell.y - chosenCell.y > 0) { // Above
                 currentCell.frontWall = false
                 chosenCell.backWall = false
-            } else if (currentCell.y - chosenCell.y < 0) {
+            } else if (currentCell.y - chosenCell.y < 0) { // Below
                 currentCell.backWall = false
                 chosenCell.frontWall = false
-            } else if (currentCell.x - chosenCell.x < 0) {
+            } else if (currentCell.x - chosenCell.x < 0) { // Right
                 currentCell.rightWall = false
                 chosenCell.leftWall = false
-            } else if (currentCell.x - chosenCell.x > 0) {
+            } else if (currentCell.x - chosenCell.x > 0) { // Left
                 currentCell.leftWall = false
                 chosenCell.rightWall = false
             }
 
+            // we make the chosen cell the current cell and mark it as visited
             currentCell = chosenCell
             chosenCell.visited = true
 
+            // we subract one, because a new one has been visited
             unvisited--
         } else
+            // We go back to the previous cell
+            // we backtrack lmao
             currentCell = stack.pop()
     }
 
-    let newCells = []
+    // Converts the 2D array to a 1D array
+    let newCells: Cell[] = []
     for (let line of cells)
         for (let cell of line)
             newCells.push(cell)
